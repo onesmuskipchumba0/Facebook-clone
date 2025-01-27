14bab95cc334c5588ba5b4d63ec19822f5083ca5
OnesmusBett <onesmuskipchumba5@gmail.com>
2025-01-27 22:54:47 +0300

    add center and right nav

--- app/page.js
+++ app/page.js
@@ -3,7 +3,10 @@ import Link from "next/link";
 import { FaUser, FaClock, FaBookmark, FaUsers, FaVideo, FaStore, FaCalendarAlt, FaChartBar } from 'react-icons/fa';
 
 export default function Home() {
+  const friends = ["John doe", "Mary Doe", "Peter", "Joshua"]
   return (
+    <div className="flex flex-row">
+      {/* Left side nav */}
     <div className="mx-3 text-white">
       <Link href={"/user"} className="flex items-center gap-2 flex-row">
         <FaUser size={24} style={{ margin: '1rem 0' }} color="#1877F2"/>
@@ -37,6 +40,21 @@ export default function Home() {
         <FaChartBar size={24} style={{ margin: '1rem 0' }} className="text-blue-800"/>
         <p>Ads Manager</p>
       </Link>
+    </div>
+
+    {/* Center main content */}
+    <div className="flex flex-col items-center">
+
+    </div>
+    {/* Right nav */}
+    <div className="items-center">
+      {friends.map((e,i)=>(
+        <div key={i} className="flex flex-row items-center gap-2">
+          <div className="bg-slate-400 rounded-full w-12 h-12 my-3"></div>
+          {e}
+        </div>
+      ))}
+    </div>
     </div>
   );
 }
